3a477e154706860c1203760331c9458b9363311c
fanfanda <fanfanda@ict.ac.cn>
2018-02-17 19:39:51 +0800

    nothing

--- tiantic.py
+++ tiantic.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 train_data=pd.read_csv("../first_train_index.csv")
-train_matrix=np.zeros(len(train_data),2600)
+train_matrix=np.zeros((len(train_data),2600))
 for i in train_data.index:
     temp=open('../train_data/'+str(train_data.loc[i,'id'])+'.txt').read()
     train_matrix[i]=np.array(eval(temp))
