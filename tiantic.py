08150e24357b79e0f98f84749f287c510a61e62d
fanfanda <fanfanda@ict.ac.cn>
2018-02-17 11:42:52 +0800

    nothing

--- tiantic.py
+++ tiantic.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 train_data=pd.read_csv("../first_train_index.csv")
+train_data['data']=''
+
+for i in train_data.index:
+    temp=open('../train_data/'+str(train_data.ix[i,'id'])+'.txt').read()
+    train_data.ix[i,'data']=eval(temp)
 
